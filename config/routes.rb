8420ebd226b00629b868846cf58abf738720b72c
TomK32 <tomk32@tomk32.de>
2008-04-17 15:53:49 +0200

    some conditions for the year/month/day route

--- config/routes.rb
+++ config/routes.rb
@@ -2,40 +2,12 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :feeds
   
   map.root :controller => 'timeline', :action => 'articles_for'
-  map.articles_for ':year/:month/:day', :controller => 'timeline', :action => 'articles_for'
-  map.login 'login', :controller => 'login', :action => 'login'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
-  # See how all your routes lay out with "rake routes"
+  map.articles_for ':year/:month/:day', :controller => 'timeline', :action => 'articles_for',
+    :year => /\d{4}/, :month => /\d{1,2}/, :day => /\d{1,2}/
 
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  
 end
