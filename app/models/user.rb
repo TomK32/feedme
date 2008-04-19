9d7d5d86f5e3e93ac5a5bbe17166a53cb83c9fe0
TomK32 <tomk32@tomk32.de>
2008-04-19 08:58:58 +0200

    added moderator? and admin? to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < Goldberg::User
+  def moderator?
+    return role.name == 'Moderator' || role.name == 'Administrator'
+  end
+  def admin?
+    return role.name == 'Administrator'
+  end
 end
