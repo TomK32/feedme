9de02d7503a8f7fabfe458bbb8752d546850fa20
TomK32 <tomk32@tomk32.de>
2008-04-19 08:59:25 +0200

    fixed timeline

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -3,12 +3,12 @@ class TimelineController < ApplicationController
   
   def articles_for
     if params[:year].nil? or params[:month].nil? or params[:day].nil?
-      @current_date = Time.parse("#{params[:year]}-#{params[:month]}-#{params[:day]}")
+        @current_date = Time.now
       @articles = Article.find :all, :order => 'published DESC', :limit => 10
     else
-        @current_date = Time.now
+        @current_date = Time.parse("#{params[:year]}-#{params[:month]}-#{params[:day]}")
         @articles = Article.find :all, :include => :feed,
-                                    :conditions => ['published BETWEEN ? AND ?', @current_date.end_of_day, @current_date.beginning_of_day],
+                                    :conditions => ['published BETWEEN ? AND ?', @current_date.beginning_of_day, @current_date.end_of_day],
                                     :order => 'published DESC'
     end
   end
